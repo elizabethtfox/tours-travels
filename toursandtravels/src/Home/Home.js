e36664b313889314f8c46c90f271f63e46614344
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-07-07 16:24:02 -0400

    created backup of original mariam branch layout

--- toursandtravels/src/Home/Home.js
+++ toursandtravels/src/Home/Home.js
@@ -35,8 +35,8 @@ class Home extends Component {
         {
           !isAuthenticated() && (
             <div className="container column">
+            <img className="bg" src={ Background } alt="Sweden" />
               <h5>Tours and Travels</h5>
-              <img src={ Background } alt="Sweden" />
               <h5>
                 You are not logged in! Please{' '}
                 <a
