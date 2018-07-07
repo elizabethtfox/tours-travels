04bf5c7993da355e9534382dd5efce2274f5bc9d
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-07-06 20:09:28 -0400

    updated list look

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Lists from "./pages/Lists";
-import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import './App.css';
 
 const App = () => (
 <Router>
 <div>
-<Nav />
 <Switch>
 <Route exact path="/" component={Lists} />
 <Route exact path="/lists" component={Lists} />
-<Route exact path="/lists/:id" component={Detail} />
 <Route component={NoMatch} />
 </Switch>
 </div>
--- client/src/pages/Lists/Lists.js
+++ client/src/pages/Lists/Lists.js
@@ -1,110 +1,56 @@
-import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
-import Jumbotron from "../../components/Jumbotron";
-import API from "../../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import Input from '../../components/Input/Input';
+import Item from '../../components/Item/Item';
 
 class Lists extends Component {
-  state = {
-    lists: [],
-    category: "",
-    items: ""
-  };
+    constructor(props){
+        super(props);
 
-  componentDidMount() {
-    this.loadLists();
-  }
+        this.state = {
+            todos: [
+                {id: 0, text: "Stretchy pants"},
+                {id: 1, text: "Raincoat"}
+            ],
+            nextId: 2
+        }
 
-  loadLists = () => {
-    API.getLists()
-      .then(res =>
-        this.setState({ lists: res.data, category: "", items: "" })
-      )
-      .catch(err => console.log(err));
-  };
-
-  deleteList = id => {
-    API.deleteList(id)
-      .then(res => this.loadLists())
-      .catch(err => console.log(err));
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.category) {
-      API.saveList({
-        category: this.state.category,
-        items: this.state.items,
-      })
-        .then(res => this.loadLists())
-        .catch(err => console.log(err));
+        this.addTodo = this.addTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
     }
-  };
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col size="md-6">
-            <Jumbotron>
-              <h1>Make a List Here</h1>
-            </Jumbotron>
-            <form>
-              <Input
-                value={this.state.category}
-                onChange={this.handleInputChange}
-                name="category"
-                placeholder="Category (required)"
-              />
-              <TextArea
-                value={this.state.items}
-                onChange={this.handleInputChange}
-                name="items"
-                placeholder="Items (Optional)"
-              />
-              <FormBtn
-                disabled={!(this.state.category)}
-                onClick={this.handleFormSubmit}
-              >
-                Submit List
-              </FormBtn>
-            </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>My Lists</h1>
-            </Jumbotron>
-            {this.state.lists.length ? (
-              <List>
-                {this.state.lists.map(list => (
-                  <ListItem key={list._id}>
-                    <Link to={"/lists/" + list._id}>
-                      <strong>
-                        {list.category}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteList(list._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
-      </Container>
+    addTodo(todoText){
+        let todos = this.state.todos.slice();
+        todos.push({id: this.state.nextId, text: todoText});
+        this.setState({
+            todos: todos,
+            nextId: ++this.state.nextId
+        });
+    }
+
+    removeTodo(id){
+        this.setState({
+            todos: this.state.todos.filter((todo, index) => todo.id !== id)
+    })
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <div className="todo-wrapper">
+            <Header />
+            <Input todoText="" addTodo={this.addTodo}/>
+        <ul>
+        {
+            this.state.todos.map((todo) => {
+            return <Item todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo} />
+    })
+    }
+    </ul>
+        </div>
+        </div>
     );
-  }
+    }
 }
 
 export default Lists;
