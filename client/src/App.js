7054ef8da692ac21fb0503ead2968a67bbc6ea6d
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-07-08 22:07:06 -0400

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Lists from "./pages/Lists";
 import Map from "./pages/Map";
+import App from "./pages/App";
 //import NoMatch from "./pages/NoMatch";
 import './App.css';
 
-const App = () => (
+const Test = () => (
 <Router>
 <div>
 <Switch>
-<Route exact path="/" component={Map} />
+<Route exact path="/" component={App} />
 <Route exact path="/lists" component={Lists} />
+<Route exact path="/map" component={Map} />
 </Switch>
 </div>
 </Router>
 );
 
-export default App;
+export default Test;
---
+++ client/src/pages/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import '@appbaseio/reactivesearch/dist/css/style.min.css';
+
+import './index.css';
+import Routes from './Routes';
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
+
+export { default } from "./App.js";
