868a1f3215ba067967ae66c6991f6757ac34e42a
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-07-12 16:14:16 -0400

    got the carousel to work

--- client/src/pages/App/App.js
+++ client/src/pages/App/App.js
@@ -1,96 +1,30 @@
-import React, { Component } from "react";
-import {
-  ReactiveBase,
-  CategorySearch,
-  RatingsFilter,
-  ResultCard
-} from '@appbaseio/reactivesearch';
+import React, { Component } from 'react';
+import './App.css';
+import {Carousel, Button, Icon} from 'react-materialize';
 
 class App extends Component {
-  onData(res) {
-    const result = {
-      image: "https://www.enterprise.com/content/dam/global-vehicle-images/cars/FORD_FOCU_2012-1.png",
-      title: res.name,
-      rating: res.rating,
-      desc: res.brand,
-      url: "#"
-    };
-    return result;
-  }
-
   render() {
     return (
-      // <ReactiveBase
-      //   app="car-store"
-      //   credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c"
-      // >
-      //   <div className="row reverse-labels">
-      //     <div className="col s6 col-xs-6">
-      //       <div className="row">
-      //         <div className="col s12 col-xs-12">
-      //           <CategorySearch
-      //             componentId="SearchSensor"
-      //             dataField="name"
-      //             categoryField="brand.raw"
-      //             placeholder="Search Cars"
-      //           />
-      //         </div>
-      //         <div className="col s12 col-xs-12">
-      //           <RatingsFilter
-      //             componentId="RatingsSensor"
-      //             dataField="rating"
-      //             title="RatingsFilter"
-      //             data={
-      //               [{ start: 4, end: 5, label: "4 stars and up" },
-      //               { start: 3, end: 5, label: "3 stars and up" },
-      //               { start: 2, end: 5, label: "2 stars and up" },
-      //               { start: 1, end: 5, label: "> 1 stars" }]
-      //             }
-      //             defaultSelected={{
-      //                 "start": 3,
-      //                 "end": 5
-      //             }}
-      //           />
-      //         </div>
-      //       </div>
-      //     </div>
-      //     <div className="col s6 col-xs-6">
-      //       <ResultCard
-      //         componentId="SearchResult"
-      //         dataField="name"
-      //         title="Results"
-      //         from={0}
-      //         size={20}
-      //         onData={this.onData}
-      //         sortOptions={[
-      //           {
-      //               label: "Lowest Price First",
-      //               dataField: "price",
-      //               sortBy: "asc"
-      //           },
-      //           {
-      //               label: "Highest Price First",
-      //               dataField: "price",
-      //               sortBy: "desc"
-      //           },
-      //           {
-      //               label: "Most rated",
-      //               dataField: "rating",
-      //               sortBy: "desc"
-      //           }
-      //         ]}
-      //         react={{
-      //             and: ["SearchSensor", "RatingsSensor"]
-      //         }}
-      //       />
-      //     </div>
-      //   </div>
-      // </ReactiveBase>
-      <div>
-        Hi </div>
-      );
-    };
+     <div> 
   
-  }
+      <div>
+      <Carousel options={{ fullWidth: false }} images={[
+        'https://images.unsplash.com/photo-1495442358998-961b69f45703?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=88ec7de7aa5e3e74217e5ddf03e28086&auto=format&fit=crop&w=2448&q=80',
+        'https://images.unsplash.com/photo-1491900177661-4e1cd2d7cce2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e5f7a670b5cd3ff8bbc7b21ef4e4461&auto=format&fit=crop&w=800&q=60',
+        'https://images.pexels.com/photos/130159/pexels-photo-130159.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+        'https://images.unsplash.com/photo-1503971090465-19d3c80f81f3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8b90203c424ce20c853f028a7110ae9e&auto=format&fit=crop&w=1745&q=80',
+        'https://images.unsplash.com/photo-1511371241697-fbcaef9dd576?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8ed2a21f5df4012664c88d55cca5ace6&auto=format&fit=crop&w=1655&q=80'
+      ]} />
 
-  export default App;
+    </div>
+    {/* <Button waves='light'>Connect with me!<Icon left>Call Made</Icon></Button> */}
+
+    </div> 
+    
+    );
+  }
+}
+
+export default App;
+
+
--- client/src/pages/App/index.js
+++ client/src/pages/App/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
 import './index.css';
 import Routes from './Routes';
+import App from './App';
+
+import registerServiceWorker from './registerServiceWorker';
+
+
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
 export { default } from "./Routes.js";
 
