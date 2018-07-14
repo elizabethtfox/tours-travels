f4fa4f4b0ce9be1aeab6e35d415a0062ae1933d7
Catherine Muthoni <cdmuthoni20@gmail.com>
2018-07-14 13:38:10 -0400

    update api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,21 +18,6 @@ export default {
     return axios.post("/api/lists", listData);
   }
 };
-// Export an object containing methods we'll use for accessing the Yelp API
-
-export default {
-  
-  getRestaurantsOfTerm: function(term, location) {
-     return axios.get("https://api.yelp.com/v3/businesses/search?term=" + term + "&location=" + location);
-    // return axios.get(BASEURL + term + "&location=" + location + APIKEY);
-
-   
-  },
-  getBaseLocationList: function() {
-    return axios.get("https://api.yelp.com/v3/businesses/list");
-  }
-};
-
 
 
  
