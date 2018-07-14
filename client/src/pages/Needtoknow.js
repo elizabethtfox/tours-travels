26b0db31c2332d43ef41434c000dc593359c5635
Catherine Muthoni <cdmuthoni20@gmail.com>
2018-07-14 10:23:39 -0400

    file updates

--- client/src/pages/Needtoknow.js
+++ client/src/pages/Needtoknow.js
@@ -3,7 +3,10 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-import logo from "../images/passport.jpg";
+import passport from "../images/passport.jpg";
+import currency from "../images/currency.jpg";
+import language from "../images/language.jpg";
+import transport from "../images/transport.jpeg";
 
 
 
@@ -20,39 +23,50 @@ const Needtoknow = () => (
           
         </Col>
       </Row>
+                    {'\n'}
       <Row>
         <Col size="md-12">
           <p>
-          Learn about specific information about your destination, safety and security information, travel advisories and visa information.
-          {'\n'}
           
-          <a href="https://travel.state.gov/content/travel/en/international-travel.html" target="_blank">
-                        <img className="img-responsive" src={"../images/passport.jpg"} width="40" alt="logo"/>
+          <a href={"https://travel.state.gov/content/travel/en/international-travel.html"} target="_blank">
+                        <img className="img-responsive" src={ passport } width="200" alt="passport"/>
                     </a>
+                    {'\n'}
+          Learn about specific information about your destination, safety and security information, travel advisories and visa information.
+          
           </p>
 
           <p>
-          Currency Converter
 
-          <a href="https://www.xe.com/currencyconverter/" target="_blank">
-                        <img className="img-responsive" src={"../images/currency.jpg"} width="40" alt="logo"/>
+            <a href="https://www.xe.com/currencyconverter/" target="_blank">
+                        <img className="img-responsive" src={currency} width="200" alt="currency"/>
                     </a>
+
+          Currency ConverterS
+
           </p>
 
+
           <p>
+
+              <a href="https://www.uber.com/" target="_blank">
+                        <img className="img-responsive" src={transport} width="200" alt="transport"/>
+                    </a>
           Transportation
 
-          <a href="https://www.uber.com/" target="_blank">
-                        <img className="img-responsive" src={"../images/transport.jpg"} width="40" alt="logo"/>
-                    </a>
+         
           </p>
 
+         
+
           <p>
-          Language Translation
 
           <a href="https://translate.google.com/" target="_blank">
-                        <img className="img-responsive" src={"../images/transport.jpg"} width="40" alt="logo"/>
+                        <img className="img-responsive" src={language} width="200" alt="language"/>
                     </a>
+          Language Translation
+
+          
             </p>
         </Col>
       </Row>
