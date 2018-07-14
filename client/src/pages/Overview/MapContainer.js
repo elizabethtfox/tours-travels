2dcef4577abe04f4b5fcb58c459682ae2d8ea3fc
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-07-14 13:29:18 -0400

    added food map

---
+++ client/src/pages/Overview/MapContainer.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+export default class MapContainer extends Component {
+
+  componentDidMount() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      const {google} = this.props;
+      const maps = google.maps;
+      var lat;
+      var long;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+      getLocation();
+
+      function getLocation() {
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(showPosition);
+          } else {
+              alert("Geolocation is not supported by this browser.");
+          }
+      }
+
+      var self = this;
+      function showPosition(position) {
+          lat =  position.coords.latitude;
+          long = position.coords.longitude;
+          var myPlace = {lat: lat, lng: long };
+          var map;
+          var markerArray = [];
+
+          const mapConfig = Object.assign({}, {
+            center: {lat: lat, lng: long},
+            zoom: 12,
+            mapTypeId: 'roadmap'
+          })
+
+          var currMap = new maps.Map(node, mapConfig);
+          var infowindow = new google.maps.InfoWindow();
+          var directionsService = new google.maps.DirectionsService;
+          var service = new google.maps.places.PlacesService(currMap);
+          var directionsDisplay = new google.maps.DirectionsRenderer({map: currMap});
+          var stepDisplay = new google.maps.InfoWindow;
+
+
+          service.nearbySearch({
+              location : myPlace,
+              radius : 10000,
+              types : [ 'restaurant' ]
+          }, callback);
+
+          function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+          }
+
+          var onChangeHandler = function(selectedPlace) {
+            calculateAndDisplayRoute(
+                directionsDisplay, directionsService, markerArray, stepDisplay, currMap, selectedPlace);
+          };
+
+          function calculateAndDisplayRoute(directionsDisplay, directionsService,
+          markerArray, stepDisplay, map, selectedPlace) {
+            for (var i = 0; i < markerArray.length; i++) {
+              markerArray[i].setMap(null);
+            }
+
+            var start = new google.maps.LatLng(lat, long);
+            var end = new google.maps.LatLng(selectedPlace.geometry.location.lat(), selectedPlace.geometry.location.lng());
+            directionsService.route({
+              origin: start,
+              destination: end,
+              travelMode: 'DRIVING'
+            },
+            function(response, status) {
+                if (status === 'OK') {
+                  directionsDisplay.setDirections(response);
+                  showSteps(response, markerArray, stepDisplay, map);
+                } else {
+                  window.alert('Directions request failed due to ' + status);
+                }
+            });
+          }
+
+          function showSteps(directionResult, markerArray, stepDisplay, map) {
+            var myRoute = directionResult.routes[0].legs[0];
+            for (var i = 0; i < myRoute.steps.length; i++) {
+              var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+              marker.setMap(map);
+              marker.setPosition(myRoute.steps[i].start_location);
+              attachInstructionText(
+                  stepDisplay, marker, myRoute.steps[i].instructions, map);
+            }
+          }
+
+          function attachInstructionText(stepDisplay, marker, text, map) {
+            google.maps.event.addListener(marker, 'click', function() {
+              stepDisplay.setContent(text);
+              stepDisplay.open(map, marker);
+            });
+          }
+
+          function createMarker(place) {
+              const marker = new maps.Marker({ // creates a new Google maps Marker object.
+                position: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()}, // sets position of marker to specified location
+                map: currMap,
+                title: place.name
+              });
+
+              google.maps.event.addListener(marker, 'click', function() {
+                var isOpen = place.opening_hours.open_now ? 'Yes' : 'No';
+                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                'Rating : ' + place.rating + '<br>' +
+                'Open Now : ' + isOpen + '<br>' +
+                place.vicinity + '</div>');
+                infowindow.open(currMap, this);
+                onChangeHandler(place);
+              });
+          }
+          self.map = currMap; // creates a new Google map on the specified node (ref='map') with the specified configuration set above.
+      }
+
+    }
+  }
+
+  render() {
+    const style = {
+      width: '90vw', // 90vw basically means take up 90% of the width screen. px also works.
+      height: '75vh' // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
+    }
+
+    return ( // in our return function you must return a div with ref='map' and style.
+      <div id="map-container">
+        <div ref="map" style={style}>
+          loading map...
+        </div>
+      </div>
+    )
+  }
+}
--- client/src/pages/Overview/Overview.js
+++ client/src/pages/Overview/Overview.js
@@ -1,36 +1,85 @@
-import React from "react";
+import React, { Component } from 'react';
 import Hero from "../../components/Hero";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
 import Col from "../../components/Col";
 
-const Overview = () => (
-  <div>
-    <Hero backgroundImage="https://images.pexels.com/photos/5249/bread-food-restaurant-people.jpg?cs=srgb&dl=beer-bread-delicious-5249.jpg&fm=jpg">    
-      {/* <h1>Pupster</h1>
-      <h2>They're the Good Boys and Girls</h2> */}
-    </Hero>
-    <Container style={{ marginTop: 30 }}>
-      <Row>
-        <Col size="md-12">
-          <h1>Eat, drink and be merry!</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="md-12">
-          <p>
-          Italian cuisine has influenced food culture around the world and is viewed as a form of art by many. Wine, cheese and pasta are important parts of Italian meals. Pasta comes in a wide range of shapes, widths and lengths, including penne, spaghetti, linguine, fusilli and lasagna. 
-          </p>
-          <p>
-          No one area of Italy eats the same things as the next. Each region has its own spin on "Italian food," according to CNN. For example, most of the foods that Americans view as Italian, such as spaghetti and pizza, come from central Italy. In the North of Italy, fish, potatoes, rice, sausages, pork and different types of cheeses are the most common ingredients. Pasta dishes with tomatoes are popular, as are many kinds of stuffed pasta, polenta and risotto. In the South, tomatoes dominate dishes, and they are either served fresh or cooked into sauce. Southern cuisine also includes capers, peppers, olives and olive oil, garlic, artichokes, eggplant and ricotta cheese.
-          </p>
-          <p>
-          Wine is also a big part of Italian culture, and the country is home to some of the world's most famous vineyards. The oldest traces of Italian wine were recently discovered in a cave near Sicily's southwest coast. "The archaeological implications of this new data are enormous, especially considering that the identification of wine [is] the first and earliest-attested presence of such product in an archaeological context in Sicily," researchers wrote in the study, published online August 2017 in the Microchemical Journal.
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+// import the Google Maps API Wrapper from google-maps-react
+import { GoogleApiWrapper } from 'google-maps-react'
+// import child component
+import MapContainer from './MapContainer'
 
-export default Overview;
+
+
+class Overview extends Component {
+    render() {
+        return (
+            <div>
+        <Hero backgroundImage="https://images.pexels.com/photos/5249/bread-food-restaurant-people.jpg?cs=srgb&dl=beer-bread-delicious-5249.jpg&fm=jpg">
+            {/* <h1>Pupster</h1>
+      <h2>They're the Good Boys and Girls</h2> */}
+            </Hero>
+            <Container style={{ marginTop: 30 }}>
+    <Row>
+        <Col size="md-12">
+            <h1>Eat, drink and be merry!</h1>
+        </Col>
+        </Row>
+        <Row>
+        <Col size="md-12">
+            <p>
+            Italian cuisine has influenced food culture around the world and is viewed as a form of art by many. Wine, cheese and pasta are important parts of Italian meals. Pasta comes in a wide range of shapes, widths and lengths, including penne, spaghetti, linguine, fusilli and lasagna.
+        </p>
+        <p>
+        No one area of Italy eats the same things as the next. Each region has its own spin on "Italian food," according to CNN. For example, most of the foods that Americans view as Italian, such as spaghetti and pizza, come from central Italy. In the North of Italy, fish, potatoes, rice, sausages, pork and different types of cheeses are the most common ingredients. Pasta dishes with tomatoes are popular, as are many kinds of stuffed pasta, polenta and risotto. In the South, tomatoes dominate dishes, and they are either served fresh or cooked into sauce. Southern cuisine also includes capers, peppers, olives and olive oil, garlic, artichokes, eggplant and ricotta cheese.
+        </p>
+        <p>
+        Wine is also a big part of Italian culture, and the country is home to some of the world's most famous vineyards. The oldest traces of Italian wine were recently discovered in a cave near Sicily's southwest coast. "The archaeological implications of this new data are enormous, especially considering that the identification of wine [is] the first and earliest-attested presence of such product in an archaeological context in Sicily," researchers wrote in the study, published online August 2017 in the Microchemical Journal.
+        </p>
+
+        <h1> Restaurant Locator </h1>
+
+        <MapContainer google={this.props.google} />
+
+        </Col>
+        </Row>
+        </Container>
+        </div>
+    );
+    }
+}
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAC-UBAmHBb8SKuwLcvT0LJaC3Upq4DCVo',
+})(Overview)
+
+
+
+//export default Overview;
+
+// const Overview = () => (
+// <div>
+// <Hero backgroundImage="https://images.pexels.com/photos/5249/bread-food-restaurant-people.jpg?cs=srgb&dl=beer-bread-delicious-5249.jpg&fm=jpg">
+//     {/* <h1>Pupster</h1>
+//       <h2>They're the Good Boys and Girls</h2> */}
+//     </Hero>
+//     <Container style={{ marginTop: 30 }}>
+// <Row>
+// <Col size="md-12">
+//     <h1>Eat, drink and be merry!</h1>
+// </Col>
+// </Row>
+// <Row>
+// <Col size="md-12">
+//     <p>
+//     Italian cuisine has influenced food culture around the world and is viewed as a form of art by many. Wine, cheese and pasta are important parts of Italian meals. Pasta comes in a wide range of shapes, widths and lengths, including penne, spaghetti, linguine, fusilli and lasagna.
+// </p>
+// <p>
+// No one area of Italy eats the same things as the next. Each region has its own spin on "Italian food," according to CNN. For example, most of the foods that Americans view as Italian, such as spaghetti and pizza, come from central Italy. In the North of Italy, fish, potatoes, rice, sausages, pork and different types of cheeses are the most common ingredients. Pasta dishes with tomatoes are popular, as are many kinds of stuffed pasta, polenta and risotto. In the South, tomatoes dominate dishes, and they are either served fresh or cooked into sauce. Southern cuisine also includes capers, peppers, olives and olive oil, garlic, artichokes, eggplant and ricotta cheese.
+// </p>
+// <p>
+// Wine is also a big part of Italian culture, and the country is home to some of the world's most famous vineyards. The oldest traces of Italian wine were recently discovered in a cave near Sicily's southwest coast. "The archaeological implications of this new data are enormous, especially considering that the identification of wine [is] the first and earliest-attested presence of such product in an archaeological context in Sicily," researchers wrote in the study, published online August 2017 in the Microchemical Journal.
+// </p>
+// </Col>
+// </Row>
+// </Container>
+// </div>
